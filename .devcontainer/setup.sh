fd5b38074f16cc38af6ea8a8b20b0c14dbe20298
Owen Barton <owen@owenbarton.com>
2026-02-11 01:47:35 +0000

    chore: Test moving worktrees to another location.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-# Set ownership on /workspaces
-sudo chown -R vscode:vscode /workspaces
-
 # Install Spec-Kit tool
 uv tool install specify-cli --from git+https://github.com/github/spec-kit.git --force
